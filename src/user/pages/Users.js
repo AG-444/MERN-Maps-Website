de43cbad95f9000b8c6e25c375b2c9f26d55eb80
= <--replace>
2025-06-20 00:00:07 +0530

    V45 Add Avatar and Card components with styles; update UserItem and UsersList for improved layout

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,7 +9,8 @@ const Users = () => {
             name:'Adi', 
             image:'https://smartwatermagazine.com/sites/default/files/styles/thumbnail-830x455/public/valley.jpg?itok=94H9MLGq', 
             places:3
-        }
+        },
+        
     ];
     return <UsersList items={USERS}/>;
 };
