deecd008b432bece67586af8ecbef2034978cf8f
= <--replace>
2025-06-19 23:00:36 +0530

    V43, Users in progress

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>User Works!</h2>
+    const USERS = [{
+        id:'u1', 
+        name:'Adi', 
+        image:'https://smartwatermagazine.com/sites/default/files/styles/thumbnail-830x455/public/valley.jpg?itok=94H9MLGq', 
+        places:3
+    }];
+    return <UsersList items={USERS}/>;
 };
 
 export default Users
