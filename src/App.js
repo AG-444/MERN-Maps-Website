217af04532f8a385e744e6aad3bb90642177ad4a
AG-444 <aditya444garg@gmail.com>
2025-06-20 10:51:57 +0530

    V47 Add MainNavigation component and enhance UsersList with Card; implement MainHeader styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import{ BrowserRouter as Router, Route , Redirect, Switch} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return <Router>
+  return( 
+  <Router>
+    <MainNavigation/>
+    <main>
     <Switch>
       <Route path="/" exact>
         <Users/>
@@ -14,7 +18,9 @@ const App = () => {
       </Route>
       <Redirect to="/"/>
     </Switch>
-  </Router>;
+    </main>
+  </Router>
+  );
 }
 
 export default App;
