8687995a741b0622ea40e46c3c8adef107252561
AG-444 <aditya444garg@gmail.com>
2025-06-20 20:49:14 +0530

    V55 Add Modal component and integrate with PlaceItem; update index.html and styles

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React,{ useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 import './PlaceItem.css'
 
 const PlaceItem = props =>{
-    return <li className="place-item">
-        <Card className="place-item__content">
-            <div className="place-item__image">
-                <img src={props.image} alt={props.title}/>
-            </div>
-            <div className="place-item__info">
-                <h2>{props.title}</h2>
-                <h3>{props.address}</h3>
-                <p>{props.description}</p>
-            </div>
-            <div className="place-item__actions">
-                <Button >View On Map</Button>
-                <Button to={`/places/${props.id}`}>Edit</Button>
-                <Button danger>Delete</Button>
-            </div>
-        </Card>
-    </li>
+    const  [ShowMap, setShowMap] = useState(false);
+
+    const openMapHandler = () => setShowMap(true);
+    const closeMapHandler = () => setShowMap(false);
+    
+    return (    
+        <React.Fragment>
+            <Modal 
+                show={ShowMap} 
+                onCancel={closeMapHandler} 
+                header={props.address}
+                className="place-item__modal-content"
+                footerClass="place-item__modal-actions"
+                footer={<Button onClick={closeMapHandler}>Close</Button>}
+            >
+                <div className="map-container">
+                    <h2>The Map!</h2>
+                </div>
+            </Modal>
+        <li className="place-item">
+            <Card className="place-item__content">
+                <div className="place-item__image">
+                    <img src={props.image} alt={props.title}/>
+                </div>
+                <div className="place-item__info">
+                    <h2>{props.title}</h2>
+                    <h3>{props.address}</h3>
+                    <p>{props.description}</p>
+                </div>
+                <div className="place-item__actions">
+                    <Button onClick={openMapHandler}>View On Map</Button>
+                    <Button to={`/places/${props.id}`}>Edit</Button>
+                    <Button danger>Delete</Button>
+                </div>
+            </Card>
+        </li>
+        </React.Fragment>
+    );
 };
 
 export default PlaceItem;
