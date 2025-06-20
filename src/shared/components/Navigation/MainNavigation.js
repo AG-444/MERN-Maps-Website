1dd26a0da3c34661cd05d9766583c677e3d48914
AG-444 <aditya444garg@gmail.com>
2025-06-20 16:53:17 +0530

    V52 Add Backdrop component and integrate with SideDrawer; update package dependencies

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import Navlinks from './NavLinks';
 import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const OpenDrawerHandler = () =>{
+    setDrawerIsOpen(true);
+  };
+
+  const CloseDrawerHandler = () =>{
+    setDrawerIsOpen(false);
+  };
+  
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className='main-navigation__drawer-nav'>
-          <Navlinks/>
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={CloseDrawerHandler}/>}
+        <SideDrawer show ={drawerIsOpen} onClick={CloseDrawerHandler}>
+          <nav className='main-navigation__drawer-nav'>
+            <Navlinks/>
+          </nav>
+        </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={OpenDrawerHandler}>
           <span />
           <span />
           <span />
