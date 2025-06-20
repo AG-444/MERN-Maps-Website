22e52c5e6026ccb1b190fdd1041654044bc44a87
AG-444 <aditya444garg@gmail.com>
2025-06-20 16:29:18 +0530

    V50 Add SideDrawer component and integrate with MainNavigation; update index.html

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,23 +3,31 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import Navlinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">LenScape</Link>
-      </h1>
-      <nav>
-        <Navlinks/>
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+          <Navlinks/>
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">LenScape</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <Navlinks/>
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
