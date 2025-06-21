c54ab1b6ad2056003209469fbd935df8a4743d3b
AG-444 <aditya444garg@gmail.com>
2025-06-21 14:41:49 +0530

    V63 Refactor Input component to include useEffect for input validation; update NewPlace page to handle input changes

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React,{useCallback} from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
+
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+
   return (
     <form className="place-form">
       <Input 
+        id="Title" 
         element="input" 
         type="text" 
         label="Title" 
         validators={[VALIDATOR_REQUIRE()]} 
         errorText = "Please enter a Valid Title"
+        onInput ={titleInputHandler}
+      />
+      <Input 
+        id="description" 
+        element="text-area" 
+        label="description" 
+        rows='7'
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]} 
+        errorText = "Please enter a Valid desription (atleast 5 characters)"
+        onInput ={descriptionInputHandler}
       />
     </form>
   );
