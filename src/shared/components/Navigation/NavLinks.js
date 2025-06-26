3d0d1999722ce45a5846620cb14fb14995843bd7
AG-444 <aditya444garg@gmail.com>
2025-06-27 02:09:13 +0530

    V78 Implement authentication context and refactor routing: add login/logout functionality and conditional rendering of routes based on authentication state.

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React,{ useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import { AuthContext } from "../../context/auth-context";
 import './NavLinks.css';
 
 const Navlinks = props =>{
+    const auth = useContext(AuthContext);
     return <ul className="nav-links">
         <li>
             <NavLink to="/" exact>All Users</NavLink>
         </li>
-        <li>
-            <NavLink to="/u1/places">My Places</NavLink>
-        </li>
-        <li>
-            <NavLink to="/places/new">New Place</NavLink>
-        </li>
-        <li>
-            <NavLink to="/auth">Login / Sign up</NavLink>
-        </li>
+        { auth.isLoggedIn && 
+            <li>
+                <NavLink to="/u1/places">My Places</NavLink>
+            </li>
+        }
+        {auth.isLoggedIn && 
+            <li>
+                <NavLink to="/places/new">New Place</NavLink>
+            </li>
+        }
+        {!auth.isLoggedIn && 
+            <li>
+                <NavLink to="/auth">Login / Sign up</NavLink>
+            </li>
+        }
+        
     </ul>
 };
 
