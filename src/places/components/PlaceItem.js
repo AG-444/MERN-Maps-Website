a81e9276db2767dd736c438e304c0b332d13d106
ag-444 <aditya444garg@gmail.com>
2025-07-12 13:17:47 +0530

    V175 Enhance image handling in forms: integrate ImageUpload component in NewPlace and Auth pages, and update form submission to handle image uploads with FormData.

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -11,99 +11,99 @@ import { useHttpClient } from '../../shared/hooks/http-hook';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
-const { isLoading, error, sendRequest, clearError } = useHttpClient();
-const auth = useContext(AuthContext);
-const [showMap, setShowMap] = useState(false);
-const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const auth = useContext(AuthContext);
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-const openMapHandler = () => setShowMap(true);
+  const openMapHandler = () => setShowMap(true);
 
-const closeMapHandler = () => setShowMap(false);
+  const closeMapHandler = () => setShowMap(false);
 
-const showDeleteWarningHandler = () => {
+  const showDeleteWarningHandler = () => {
     setShowConfirmModal(true);
-};
+  };
 
-const cancelDeleteHandler = () => {
+  const cancelDeleteHandler = () => {
     setShowConfirmModal(false);
-};
+  };
 
-const confirmDeleteHandler = async () => {
+  const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
     try {
-    await sendRequest(
+      await sendRequest(
         `http://localhost:5000/api/places/${props.id}`,
         'DELETE'
-    );
-    props.onDelete(props.id);
+      );
+      props.onDelete(props.id);
     } catch (err) {}
-};
+  };
 
-return (
+  return (
     <React.Fragment>
-    <ErrorModal error={error} onClear={clearError} />
-    <Modal
+      <ErrorModal error={error} onClear={clearError} />
+      <Modal
         show={showMap}
         onCancel={closeMapHandler}
         header={props.address}
         contentClass="place-item__modal-content"
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
-    >
+      >
         <div className="map-container">
-        <Map center={props.coordinates} zoom={16} />
+          <Map center={props.coordinates} zoom={16} />
         </div>
-    </Modal>
-    <Modal
+      </Modal>
+      <Modal
         show={showConfirmModal}
         onCancel={cancelDeleteHandler}
         header="Are you sure?"
         footerClass="place-item__modal-actions"
         footer={
-        <React.Fragment>
+          <React.Fragment>
             <Button inverse onClick={cancelDeleteHandler}>
-            CANCEL
+              CANCEL
             </Button>
             <Button danger onClick={confirmDeleteHandler}>
-            DELETE
+              DELETE
             </Button>
-        </React.Fragment>
+          </React.Fragment>
         }
-    >
+      >
         <p>
-        Do you want to proceed and delete this place? Please note that it
-        can't be undone thereafter.
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone thereafter.
         </p>
-    </Modal>
-    <li className="place-item">
+      </Modal>
+      <li className="place-item">
         <Card className="place-item__content">
-        {isLoading && <LoadingSpinner asOverlay />}
-        <div className="place-item__image">
-            <img src={props.image} alt={props.title} />
-        </div>
-        <div className="place-item__info">
+          {isLoading && <LoadingSpinner asOverlay />}
+          <div className="place-item__image">
+            <img src={`http://localhost:5000/${props.image}`} alt={props.title} />
+          </div>
+          <div className="place-item__info">
             <h2>{props.title}</h2>
             <h3>{props.address}</h3>
             <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
+          </div>
+          <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>
-            VIEW ON MAP
+              VIEW ON MAP
             </Button>
-            {auth.userId===props.creatorId && (
-            <Button to={`/places/${props.id}`}>EDIT</Button>
+            {auth.userId === props.creatorId && (
+              <Button to={`/places/${props.id}`}>EDIT</Button>
             )}
 
-            {auth.userId===props.creatorId && (
-            <Button danger onClick={showDeleteWarningHandler}>
+            {auth.userId === props.creatorId && (
+              <Button danger onClick={showDeleteWarningHandler}>
                 DELETE
-            </Button>
+              </Button>
             )}
-        </div>
+          </div>
         </Card>
-    </li>
+      </li>
     </React.Fragment>
-);
+  );
 };
 
 export default PlaceItem;
